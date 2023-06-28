e980f84a083fec43e85e313edd4002010a1d4a03
jpgiltest <131398116+jpgiltest@users.noreply.github.com>
2023-06-28 10:59:18 -0700

    A few more updates

---
+++ public/app.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const app = firebase.app();
+    const db = firebase.firestore();
+    const myPost = db.collection('posts').doc('firstpost');
+
+
+    myPost.onSnapshot(doc => {
+        const data = doc.data();
+        document.write(data.title + `<br>`);
+        document.write(data.createdAt + `<br>`);
+        document.write(data.views + `<br>`);
+        document.write('------' + `<br>`);
+    })
+
+    /*
+    myPost.get()
+        .then(doc => {
+            const data = doc.data();
+            document.write(data.title + `<br>`);
+            document.write(data.createdAt + `<br>`);
+            document.write(data.views);
+        })
+            
+    */
+    console.log(app);
+
+});
+
+function googleLogin()  {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithPopup(provider)
+        .then(result => {
+            const user = result.user;
+            document.write(`Hello ${user.displayName}`);
+            console.log(user);
+        })
+        .catch(console.log)
+}
